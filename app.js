ae88a6cff3af2f9b64400e127ee7fbcb353998a4
An minsun <elleto123456@gmail.com>
2024-10-19 00:53:05 +0900

    feat: 나이대별 단어api

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors()) 
+
+const w = require('./ages.json')
+
+app.get('/age/:age', (req, res)=>{
+  const{age} = req.params
+  const word = w.words[`${age}대`]
+
+  if(word){
+    console.log(`나이대: ${age}대, 단어: ${word}`)
+    res.json({'age':`${age}대`, 'word':word})
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

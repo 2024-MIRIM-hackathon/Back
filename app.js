16a04e109efc2cb1aee923e0aede5a99313031e2
An minsun <elleto123456@gmail.com>
2024-10-19 03:17:53 +0900

    feat: 북마크 추가/조회 api

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ app.use(express.json());
 
 // 기본 라우트 설정
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+   res.send('2024-MITHON');
 });
 
 const w = require('./ages.json')
 
+// 나이에 따른 단어 반환
 app.get('/age/:age', (req, res)=>{
   const{age} = req.params
   const word = w.words[`${age}대`]
@@ -19,13 +20,16 @@ app.get('/age/:age', (req, res)=>{
     console.log(`나이대: ${age}대, 단어: ${word}`)
     res.json({'age':`${age}대`, 'word':word})
   }
-})
+  else {
+    res.status(404).json({ error: '해당 나이대의 단어를 찾을 수 없습니다.' });
+    }
+});
 
-const w = require('./word.json');
+const wordData = require('./word.json');
 
 // 모든 단어 반환
 app.get('/myword', (req, res) => {
-    const words = Object.keys(w);
+    const words = Object.keys(wordData);
     res.json(words);
 });
 
@@ -33,7 +37,7 @@ app.get('/myword', (req, res) => {
 app.get('/myword/:word', (req, res) => {
     const { word } = req.params;
     console.log(`Requested word: ${word}`);
-    const wordInfo = w[word];
+    const wordInfo = wordData[word];
     console.log(`Word info: ${JSON.stringify(wordInfo)}`);
 
     if (wordInfo) {
@@ -47,6 +51,28 @@ app.get('/myword/:word', (req, res) => {
     }
 });
 
+// 북마크 저장을 위한 배열
+let bookmarks = []
+
+// 북마크 추가(POST)
+app.post('/bookmarks', (req,res)=>{
+    const{word} = req.body
+    
+    // 중복된 단어는 북마크하지 않도록 체크
+    const existingBookmark = bookmarks.find(b => b.word === word)
+    if(existingBookmark){
+        return res.status(400).json({error: '이미 북마크된 단어입니다.'})
+    } 
+
+    const newBookmark = {id: bookmarks.length + 1, word}
+    bookmarks.push(newBookmark)
+    res.status(201).json(newBookmark)
+})
+
+// 모든 북마크 조회(GET)
+app.get('/bookmarks', (req,res)=>{
+    res.status(200).json(bookmarks)
+})
 // 서버 시작
 app.listen(PORT, () => {
     console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);

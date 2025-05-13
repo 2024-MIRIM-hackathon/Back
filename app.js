b7591637dd38566869f41a185c994be5b0deafc0
An minsun <elleto123456@gmail.com>
2025-05-14 01:42:23 +0900

    [feat] 단어 전체 조회 및 북마크 토글/조회 api 구현

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-const express = require('express');
-const session = require('express-session');
-const cors = require('cors');
+const express = require("express");
+const session = require("express-session");
+const cors = require("cors");
 const app = express();
 
 // 라우터 설정
-const WordDictionary = require('./routers/word_dictionary_api');
-const Quize = require('./routers/quiz_api');
-const User = require('./routers/user_api');
+const WordDictionary = require("./routers/word_dictionary_api");
+const Quize = require("./routers/quiz_api");
+const User = require("./routers/user_api");
 
-app.set('port', process.env.PORT || 5000);
-app.use(cors({
-    credentials: true
-}));
-app.use(session({
+app.set("port", process.env.PORT || 5000);
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(
+  session({
     secret: process.env.COOKIE_SECRE,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: false
-    }
-}));
+      secure: false,
+    },
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // 라우터 불러오기
-app.use('/api/worddictionary', WordDictionary);
-app.use('/api/quize', Quize);
-app.use('/api/user', User);
+app.use("/api/worddictionary", WordDictionary);
+app.use("/api/qize", Quize);
+app.use("/api/user", User);
 
 // 기본 라우트 설정
-app.get('/', (req, res) => {
-   res.send('2024-MITHON');
+app.get("/", (req, res) => {
+  res.send("2024-MITHON");
 });
 
 // 서버 실행
-const port = app.get('port');
+const port = app.get("port");
 app.listen(port, () => {
-    console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
 });
--- routers/word_dictionary_api.js
+++ routers/word_dictionary_api.js
@@ -1,54 +1,67 @@
 /* 단어 사전 api */
-const express = require('express');
-const Word = require('../word.json');
+const express = require("express");
+const db = require("../config/db");
 
 const router = express.Router();
 
 // 모든 단어 반환
-router.get('/word', (req, res) => {
-    const words = Object.keys(Word);
-    res.json(words);
+router.get("/words", async (req, res) => {
+  const sql = "SELECT word, meaning, example FROM words";
+
+  try {
+    const [rows] = await db.query(sql); // rows : 쿼리 결과로 나온 데이터(각 행) 목록.
+    res.status(200).json(rows);
+  } catch (err) {
+    console.error("단어 목록 조회 오류 : ", err);
+    return res.status(500).json({ error: "서버 오류" });
+  }
 });
 
-// 단어의 뜻과 예문을 반환
-router.get('/word/:word', (req, res) => {
-    const { word } = req.params;
-    console.log(`Requested word: ${word}`);
-    const wordInfo = Word[word];
-    console.log(`Word info: ${JSON.stringify(wordInfo)}`);
+// 북마크 추가/삭제
+router.post("/bookmarks", async (req, res) => {
+  const { userId, wordId } = req.body;
 
-    if (wordInfo) {
-        res.json({
-            word: word,                // 단어
-            meaning: wordInfo.meaning, // 뜻
-            example: wordInfo.example  // 예문
-        });
+  try {
+    const [rows] = await db.query(
+      "SELECT * FROM bookmarks WHERE user_id = ? AND word_id = ?",
+      [userId, wordId]
+    );
+
+    if (rows.length > 0) {
+      await db.query(
+        "DELETE FROM bookmarks WHERE user_id = ? AND word_id = ?",
+        [userId, wordId]
+      );
+      res.status(200).json({ message: "북마크 삭제" });
     } else {
-        res.status(404).json({ error: '단어를 찾을 수 없습니다.' });
+      await db.query("INSERT INTO bookmarks (user_id, word_id) VALUES (?, ?)", [
+        userId,
+        wordId,
+      ]);
+      res.status(200).json({ message: "북마크 추가" });
     }
+  } catch (err) {
+    console.error("북마크 토글 오류 :", err);
+    res.status(500).json({ error: "서버 오류" });
+  }
 });
 
-// TODO : DB에 저장하는 방식으로 수정해야함
-let bookmarks = [];
+// 사용자 북마크 목록 조회
+router.get("/bookmarks/:userId", async (req, res) => {
+  const userId = req.params.userId;
 
-// 북마크 추가(POST)
-router.post('/bookmarks', (req,res)=>{
-    const{word} = req.body;
-    
-    // 중복된 단어는 북마크하지 않도록 체크
-    const existingBookmark = bookmarks.find(b => b.word === word)
-    if(existingBookmark){
-        return res.status(400).json({error: '이미 북마크된 단어입니다.'});
-    } 
-
-    const newBookmark = {id: bookmarks.length + 1, word};
-    bookmarks.push(newBookmark);
-    res.status(201).json(newBookmark);
-});
-
-// 모든 북마크 조회(GET)
-router.get('/bookmarks', (req,res)=>{
-    res.status(200).json(bookmarks);
+  try {
+    const [rows] = await db.query(
+      `SELECT w.word, w.meaning, w.example FROM bookmarks b
+      JOIN words w ON b.word_id = w.id
+      WHERE b.user_id = ?`,
+      [userId]
+    );
+    res.status(200).json({ bookmarks: rows });
+  } catch (err) {
+    console.error("북마크 조회 오류 : ", err);
+    res.status(500).json({ error: "서버 오류" });
+  }
 });
 
 module.exports = router;
